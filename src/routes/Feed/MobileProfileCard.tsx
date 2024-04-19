1b0fade388aa6281c4f5353ffa71a617ac17c35e
quotuo <severus@t.edu.vn>
2024-04-19 16:22:51 +0800

    Update MobileProfileCard.tsx

--- src/routes/Feed/MobileProfileCard.tsx
+++ src/routes/Feed/MobileProfileCard.tsx
@@ -10,7 +10,7 @@ type Props = {
 const MobileProfileCard: React.FC<Props> = () => {
   return (
     <StyledWrapper>
-      <div className="top">💻 Profile</div>
+      <div className="top">🛀 Profile</div>
       <div className="mid">
         <div className="wrapper">
           <Image
@@ -59,7 +59,6 @@ const StyledWrapper = styled.div`
         > .top {
           font-size: 1.25rem;
           line-height: 1.75rem;
-          font-style: italic;
           font-weight: 700;
         }
         > .mid {
